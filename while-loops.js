bbd86c84b40046133f1fc952d519ced3f0f6a88e
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-09-29 12:27:15 -0400

    first commit

---
+++ while-loops.js
@@ -0,0 +1,86 @@
+//While Loops
+// let age = 15
+// while  (age < 25) {
+//     console.log("I can't rent a car with a deposit")
+//     console.log(true)
+//     age+=2
+// }
+// let a = 1 
+// while (a < 7){
+//     if (a % 2 === 0){
+//         console.log(a + " is even")
+//     } else {
+//         console.log(a + " is odd")
+//     }
+//     a++
+// }
+
+// let cd = 10;
+
+// while (cd >= 0) {
+//     if (cd > 0) {
+//     console.log(cd + " is greater than 0")
+//     } else {
+//         console.log(cd + " is equal to 0")
+//     }
+//     cd--
+// }
+
+// let i = 1
+// while (i < 20) {
+//     if (i % 2 === 0){
+        
+//     console.log(i * 3 + " is divisble by 2")
+//     } 
+//     i++
+// }
+
+
+// Lab:
+
+//Question 1
+// let grade = Math.floor(Math.random() * 100)
+// if (grade >= 80) {
+//     console.log("You did a good job!")
+// } else if(grade >= 70) {
+//     console.log("Keep trying")
+// } else if(grade >= 60) {
+//     console.log("Ehhhh")
+// } else if(grade >= 55) {
+//     console.log("Not to good")
+// } else {
+//     console.log("Bad grade but you'll get em next time")
+// }
+
+//Question 2
+// let num = []
+// for (i = 10; i <= 100; i+=20){
+//     num.push(i)
+// }
+// for (i = 20; i <= 100; i+=20){
+//     num.push(i)
+// }
+// console.log(num)
+
+//Question 3
+// let num1 = []
+// i = 10
+// i1 = 20
+// while (i < 100){
+//     num1.push(i)
+//     i+=20
+// } while (i1 <= 100){
+//     num1.push(i1)
+//     i1+=20
+// } 
+
+// console.log(num1)
+
+// Question 4
+// let num2 = 500
+// while (num2 <= 800){
+//     if (num2 % 4 === 0){
+//         console.log(num2)
+//     }
+//     num2++
+// }
